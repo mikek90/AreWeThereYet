80a8b927d50c3681e1569f80973348ebc7b48eea
Konrad Oleksiuk <konole@gmail.com>
2017-12-02 20:29:02 +0100

    Refactor Sproxy

--- app/controllers/sproxy_controller.rb
+++ app/controllers/sproxy_controller.rb
@@ -1,11 +1,11 @@
 class SproxyController < ApplicationController
   def show
     uri = URI.parse(request.env["QUERY_STRING"])
-    query = CGI.parse(uri.query).to_a
-    query << ["token", ["Ebbfu9vkBVFhUI3c7qOgkGjDGPZBTvAr1clgih9kDXtZp7CEEQxx8Vlx_9Kfkfg5ljGiMULII3Mkb_7SUM141FDTI6yFR8v2h1jYw3kIlLg0kunQLGSPwXFWMPS1_O5_eZnYMvQBYwba2ryFVYqcYg.."]]
-    query = query.map {|x| x.join("=") }.join("&")
 
-    uri.query = query
+    query = Rack::Utils.parse_nested_query(uri.query)
+    query["token"] = "Ebbfu9vkBVFhUI3c7qOgkGjDGPZBTvAr1clgih9kDXtZp7CEEQxx8Vlx_9Kfkfg5ljGiMULII3Mkb_7SUM141FDTI6yFR8v2h1jYw3kIlLg0kunQLGSPwXFWMPS1_O5_eZnYMvQBYwba2ryFVYqcYg.."
+
+    uri.query = Rack::Utils.build_nested_query(query)
 
     resp = RestClient.get(uri.to_s)
 
